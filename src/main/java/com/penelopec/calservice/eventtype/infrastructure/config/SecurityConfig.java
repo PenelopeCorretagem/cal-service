c88d51c77d988d9155bdbe0f7fc960b1781dd14b
KennerLima <kenner.lima@sptech.school>
2026-05-24 20:16:40 -0300

    fix: corrige import HttpMethod no SecurityConfig

--- src/main/java/com/penelopec/calservice/eventtype/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/penelopec/calservice/eventtype/infrastructure/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.penelopec.calservice.eventtype.infrastructure.config.properties.CorsP
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
